4cec5d792ac8131ae3ab4b115117ab1df8948a32
Joshua Spence <josh@joshuaspence.com>
2012-06-25 07:28:26 +1000

    Improvements to script, mainly to suppress MATLAB output and write to a file instead.

--- Matlab/josh_profile.m
+++ Matlab/josh_profile.m
@@ -37,6 +37,7 @@ profiling_root_dir = 'Profiling';
 % Iterate over all data sets
 for d = 1 : length(data)
     dataset = data(d);
+    dataset_file = strcat(dataset, '.csv');
     
     % Iterate over all iterations
     for j = 1 : iterations
@@ -46,28 +47,29 @@ for d = 1 : length(data)
         for k = 1 : length(profiles_func)
             profile_name = profiles_name(k);
             profile_func = profiles_func(k);
-
             output_dir = char(strcat(profiling_root_dir, filesep, profile_name, filesep, dataset, filesep, int2str(j), filesep));
             
-            str = sprintf('Processing iteration %d of data set "%s" for profile "%s".\nTo be executed using function "%s"\nTo be output to directory "%s"', j, char(dataset), char(profile_name), char(profile_func), output_dir);
+            disp();
+            str = sprintf('Processing iteration %d of data set "%s" for profile "%s".\nFunction = "%s"\nOutput directory = "%s"', j, char(dataset), char(profile_name), char(profile_func), output_dir);
             disp(str);
 
-            % if output_dir already exists, then we don't need to profile
-            % anything
+            % If output_dir already exists, then we don't need to profile anything
             if exist(output_dir, 'dir') ~= 0
                 disp('Skipping as it appears that this data set has been profiled previously.');
                 continue;
             end
 
             % profile execution
+            disp('Running MATLAB command.');
             profile on;
-            evalc(commute_distance_anomaly(strcat(dataset, '.csv'), rerandomize, profile_func));
+            matlab_output = evalc(commute_distance_anomaly(dataset_file, rerandomize, profile_func));
             profile off;
 
             rerandomize = false;
             
             % Save profiler report
             % NOTE: This throws an error for some reason
+            disp('Saving profiler data.');
             try
                 profsave(profile('info'), output_dir);
             catch exception
@@ -77,18 +79,32 @@ for d = 1 : length(data)
             %p = profile('info');
             %save(char(strcat(output_dir, 'profiledata.mat')), p);
             %clear(p);
+            
+            % Save MATLAB output
+            disp('Saving MATLAB output.');
+            save(strcat(output_dir, filesep, 'matlab_output.log'), 'matlab_output', '-ASCII');
 
-            % save variables
+            % Save variables
+            disp('Copying output files.');
             copyfile('output.csv', output_dir);
             copyfile('random.mat', output_dir);
-            copyfile('variables.mat', output_dir);
-            copyfile('variables.txt', output_dir);
+            copyfile('graph.mat', output_dir);
+            copyfile('graph.txt', output_dir);
+            copyfile('TopN_Outlier_Pruning_Block.mat', output_dir);
+            copyfile('TopN_Outlier_Pruning_Block.txt', output_dir);
 
             % save the graph
+            disp('Saving the graph.');
             print('-dpng', strcat(output_dir, 'output.png'));
 
-            % delete the output CSV file
-            delete('output.csv', 'variables.mat', 'variables.txt');
+            % Delete the output CSV file
+            disp('Deleting output files.');
+            delete('output.csv');
+            delete('random.mat');
+            delete('graph.mat');
+            delete('graph.txt');
+            delete('TopN_Outlier_Pruning_Block.mat');
+            delete('TopN_Outlier_Pruning_Block.txt');
         end
     end
 end
