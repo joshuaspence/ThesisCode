e647cf3b1f852284b428415d2483e63c8d6c762b
Joshua Spence <josh@joshuaspence.com>
2012-07-16 09:48:23 +1000

    Made some changes to MATLAB version of TopN_Outlier_Pruning_Block to more accurately reflect the algorithm presented in the original paper.

--- TopN_Outlier_Pruning_Block/Matlab/TopN_Outlier_Pruning_Block.m
+++ TopN_Outlier_Pruning_Block/Matlab/TopN_Outlier_Pruning_Block.m
@@ -87,64 +87,53 @@ function [outliers,outlier_scores] = TopN_Outlier_Pruning_Block_IMPROVED(data, k
 
 	cutoff = 0;
 	count = 0;
-	while (n-count > 0)
-		B = zeros(1, block_size);
-		B = (count+1 : count+block_size);
-		count = count + block_size;        
-		
-		if count <= n
-			sizeB = block_size;
+	while (n-count > 0) % load a block of examples from D
+	    if count <= n
+			actual_block_size = block_size;
 		else
-			sizeB = n - (count-block_size);
+			actual_block_size = n - count;
 		end
-		neighbours = zeros(sizeB, k);
-		neighbours_dist = zeros(sizeB, k);
-		score = zeros(1, sizeB);
-
-		found = zeros(1, sizeB);
-		for i = 1 : n
-			for j = 1 : sizeB
-				if i ~= B(j) && B(j) ~= 0
-					d = dist(data(i,:), data(B(j),:))^2;
+		block = zeros(1, actual_block_size);
+		block = (count+1 : count+actual_block_size);
+		count = count + actual_block_size;        
+		
+		neighbours      = zeros(actual_block_size, k);
+		neighbours_dist = zeros(actual_block_size, k);
+		score           = zeros(1,                 actual_block_size);
+		found           = zeros(1,                 actual_block_size);
+		for vector1_index = 1 : n % for each d in D
+			for block_index = 1 : actual_block_size % for each b in B
+			    vector2_index = B(block_index);
+			    
+				if vector1_index ~= vector2_index && vector2_index ~= 0
+					d = dist(data(vector1_index,:), data(vector2_index,:))^2;
 					%fprintf('dist=%e\n', d);
 		            
-					if found(j) < k
-						found(j) = found(j)+1;
+		            [maxd, maxi] = max(neighbours_dist(vector2_index,:));
+					if found(vector2_index) < k || d < maxd % if |Neighbours(b)| < k or distance(b,d) < maxdist(b,Neighbours(b))
+						found(vector2_index) = found(vector2_index)+1;
 						
-						neighbours(j, found(j)) = i;
-						neighbours_dist(j, found(j)) = d;
+						if (found(vector2_index) < k)
+						    neighbours     (vector2_index, found(vector2_index)) = vector1_index;
+						    neighbours_dist(vector2_index, found(vector2_index)) = d;
+						else
+						    % replace the farthest point
+							neighbours     (vector2_index, maxi) = vector1_index;
+							neighbours_dist(vector2_index, maxi) = d;
+						end 
 						
-						if found(j) == k
-							score(j) = mean(neighbours_dist(j,:), 2);
-							disp(neighbours(j,:));
-							disp(neighbours_dist(j,:));
+						% update the score
+						score(vector2_index) = (score(vector2_index)*k - maxd + d)/k;
+						if score(vector2_index) <= 0
+							% avoid round off error
+							score(vector2_index) = max(mean(neighbours_dist(vector2_index,:),2), 0);
 						end
-						fprintf('no need to replace\n');
-					else % found > k
-						% find the farthest point
-						[maxd, maxi] = max(neighbours_dist(j,:));
-						% replace the farthest point
-						if d < maxd 
-							fprintf('replacing %d (%f) with %d (%f)\n', neighbours(j, maxi), neighbours_dist(j, maxi), i, d);
-							neighbours(j, maxi) = i;
-							neighbours_dist(j, maxi) = d;
-							% update the score
-							score(j) = (score(j)*k - maxd + d)/k;
-							if score(j) <= 0
-								% avoid round off error
-								score(j) = max(mean(neighbours_dist(j,:),2), 0);
-							end
-							disp(neighbours(j,:));
-							disp(neighbours_dist(j,:));
-						end            
+						
+						if score(vector2_index) < cutoff
+						    block(vector2_index) = 0;             
+						    score(vector2_index) = 0;         
+					    end          
 					end
-		            
-					if found(j) >= k && score(j) < cutoff
-						B(j) = 0;                    
-						score(j) = 0;  
-						fprintf('should skip\n');             
-					end
-					fprintf('i=%d, j=%d, B(j)=%d, found=%d, score=%e\n', i, j, B(j), found(j), score(j));
 				end
 			end
 		end
