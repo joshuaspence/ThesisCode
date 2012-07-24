4ff9e638b6220b577b15c0af4e7046f790ed23e5
Joshua Spence <josh@joshuaspence.com>
2012-07-24 10:27:04 +1000

    Added more thorough testing to josh_test.m

--- Matlab/josh_test.m
+++ Matlab/josh_test.m
@@ -15,10 +15,10 @@ data = {
 %	'segmentation', ...
 %	'spam_train', ...
 	'testCD', ...
-	'testCDST', ...
-	'testCDST2', ...
-	'testCDST3', ...
-	'testoutrank', ...
+%	'testCDST', ...
+%	'testCDST2', ...
+%	'testCDST3', ...
+%	'testoutrank', ...
 %	'spam', ...
 %	'connect4', ...
 %	'pendigits', ...
@@ -26,10 +26,10 @@ data = {
 	};
 
 % The name of the file that the "commute_distance_anomaly" script saves  
-% output variables to... from commute_distance_anomaly.m
+% output variables to... from "commute_distance_anomaly.m"
 results_file_name = 'TopN_Outlier_Pruning_Block.mat';
 
-% Each data set will be profiled with each of the following profiles
+% Each data set will be profiled with each of the following profiles.
 base_profile = struct('name', 'original',  'func', 'TopN_Outlier_Pruning_Block_ORIGINAL');
 profiles = [
 	struct('name', 'improved',  'func', 'TopN_Outlier_Pruning_Block_IMPROVED'), ...
@@ -39,17 +39,17 @@ profiles = [
 % Number of iterations for each data set
 iterations = 1;
 
-% Root output directory
+% Root output directory. If this directory exists, it will be deleted.
 testing_root_dir = strcat('.', filesep, 'test');
 if exist(testing_root_dir, 'dir') == 7
 	rmdir(testing_root_dir, 's');
 end
 mkdir(testing_root_dir);
 
-% Number of failed tests
+% Number of failed tests.
 num_failures = 0;
 
-% Iterate over all data sets
+% Iterate over all data sets.
 for d = 1 : length(data)
     dataset      = char(data(d));
     dataset_file = strcat(data_dir, filesep, dataset, '.csv');
@@ -58,14 +58,14 @@ for d = 1 : length(data)
     	mkdir(output_dir);
     end
     
-    % Iterate over all iterations
+    % Iterate over all iterations.
     for j = 1 : iterations
     	output_dir = strcat(output_dir, filesep, int2str(j));
     	if exist(output_dir, 'dir') ~= 7
     		mkdir(output_dir);
     	end
     
-    	% Randomize data
+    	% Randomize data.
     	randomness_file = strcat(output_dir, filesep, 'random.mat');
     	if exist(randomness_file, 'file') ~= 2
 			fprintf('\nRandomizing...\n');
@@ -76,7 +76,7 @@ for d = 1 : length(data)
         	fprintf('\nUsing randomness from file...\n');
         end
         
-        % Iterate over all profiles
+        % Iterate over all profiles.
         all_profiles = [base_profile, profiles];
         for k = 1 : length(all_profiles)
             profile_name       = all_profiles(k).name;
@@ -104,17 +104,18 @@ for d = 1 : length(data)
             fclose(fid);
         end
         
-        % Compare results
+        % Compare results.
         base_results_file = strcat(output_dir, filesep, base_profile.name, filesep, results_file_name);
 		base_results = load(base_results_file);
 
-		for k = 1 : length(profiles)
+        for k = 1 : length(profiles)
             profile_name = profiles(k).name;
             fprintf('\nComparing results for "%s"...\n', profile_name);
             results_file = strcat(output_dir, filesep, profile_name, filesep, results_file_name);
             results = load(results_file);
             
-            % A larger value for OF_sum is better
+            % Compare OF_sum
+            % A larger value for OF_sum is better...
         	var = char('OF_sum');
         	fprintf('Checking variable "%s"... ', var);
             if base_results.(var) <= results.(var)
@@ -122,7 +123,35 @@ for d = 1 : length(data)
            	else
            		fprintf('NO\n');
            		num_failures = num_failures + 1;
-           	end
+            end
+            fprintf('%s:\t%d\n', base_profile.name, base_results.(var));
+            fprintf('%s:\t%d\n', profile_name, results.(var));
+            
+             % Compare similarity of O.
+        	var = char('O');
+        	fprintf('Checking variable "%s"... ', var);
+            similarity = sum(ismember(base_results.(var), results.(var)));
+            size = size(base_results.(var),2);
+            fprintf('%d out of %d similar\n', similarity, size);
+            
+             % Compare range of OF.
+        	var = char('OF');
+        	fprintf('Checking variable "%s"... ', var);
+            base_upper    = base_results.(var)(1);
+            results_upper = results.(var)(1);
+            base_lower    = base_results.(var)(size(base_results.(var),2));
+            results_lower = results.(var)(size(results.(var),2));
+            
+            if (base_upper <= results_upper) && (base_lower <= results_lower)
+            	fprintf('YES\n');
+           	else
+           		fprintf('NO\n');
+           		num_failures = num_failures + 1;
+            end
+            fprintf('%s (lowest):\t%d\n', base_profile.name, base_lower);
+            fprintf('%s (highest):\t%d\n', base_profile.name, base_upper);
+            fprintf('%s (lowest):\t%d\n', profile_name, results_lower);
+            fprintf('%s (upper):\t%d\n', profile_name, results_upper);
         end
     end
 end
