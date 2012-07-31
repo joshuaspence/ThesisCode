1821fdf20307f3c14d3656fa13c91aa9c836fc55
Joshua Spence <josh@joshuaspence.com>
2012-07-31 20:46:36 +1000

    Added a minor comment.

--- TopN_Outlier_Pruning_Block/C/top_n_outlier_pruning_block.h
+++ TopN_Outlier_Pruning_Block/C/top_n_outlier_pruning_block.h
@@ -1,7 +1,7 @@
 #ifndef TOP_N_OUTLIER_PRUNING_BLOCK_H_
 #define TOP_N_OUTLIER_PRUNING_BLOCK_H_
 
-#include "macros.h"
+#include "macros.h" /* for ARRAY_SIGNATURE, VECTOR_SIGNATURE */
 
 void top_n_outlier_pruning_block(const double_t * const ARRAY_SIGNATURE(data),
                                  const size_t k, const size_t N, const size_t block_size,
