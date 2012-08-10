054d6d215e749575dc160f257639f9726f475525
Joshua Spence <josh@joshuaspence.com>
2012-08-11 07:59:19 +1000

    Merged two cleaning scripts.

--- MATLAB/clean-testing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Cleaning..."
-rm --force --verbose josh_test.log josh_test.*.log
-rm --force --verbose --recursive test/ test.*/
-echo "DONE!"
-exit 0
--- MATLAB/clean.sh
+++ MATLAB/clean.sh
@@ -2,5 +2,9 @@
 
 echo "Cleaning..."
 rm --force --verbose graph.{mat,txt} output.{csv,png} random.{mat,txt} TopN_Outlier_Pruning_Block.{mat,txt}
+
+echo "Cleaning testing files..."
+rm --force --verbose josh_test.log josh_test.*.log
+rm --force --verbose --recursive test/ test.*/
 echo "DONE!"
 exit 0
