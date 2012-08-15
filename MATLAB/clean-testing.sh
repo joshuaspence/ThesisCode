e3a446ff0e5281b03da06749518017e48382c3bb
Joshua Spence <josh@joshuaspence.com>
2012-08-16 08:32:06 +1000

    Minor changes to cleaning script outputs.

--- MATLAB/clean-testing.sh
+++ MATLAB/clean-testing.sh
@@ -3,4 +3,3 @@
 echo "Cleaning testing files..."
 rm --force --verbose josh_test.log josh_test.*.log top_n_outlier_pruning_block.log
 rm --force --verbose --recursive test/ test.*/
-echo "DONE!"
--- MATLAB/clean.sh
+++ MATLAB/clean.sh
@@ -2,4 +2,3 @@
 
 echo "Cleaning..."
 rm --force --verbose graph.{mat,txt} output.{csv,png} random.{mat,txt} TopN_Outlier_Pruning_Block.{mat,txt} vars.dat top_n_outlier_pruning_block.log
-echo "DONE!"
