3f01705657f116181a0d328f5b0526a4569bdf2a
Joshua Spence <josh@joshuaspence.com>
2012-08-19 21:55:45 +1000

    Consolidated cleaning scripts.

--- MATLAB/clean.sh
+++ MATLAB/clean.sh
@@ -1,4 +1,76 @@
 #!/bin/bash
 
-echo "Cleaning..."
-rm --force --verbose graph.{mat,txt} output.{csv,png} random.{mat,txt} TopN_Outlier_Pruning_Block.{mat,txt} vars.dat top_n_outlier_pruning_block.log
+#===============================================================================
+# Modes
+#===============================================================================
+
+clean_matlab() {
+    echo "Cleaning..."
+    rm --force --verbose graph.{mat,txt} output.{csv,png} random.{mat,txt} TopN_Outlier_Pruning_Block.{mat,txt} vars.dat top_n_outlier_pruning_block.log
+}
+
+clean_profiling() {
+    echo "Cleaning profiling data..."
+    rm --force --verbose josh_profile{,.*}.log josh_profile_block_size{,.*}.log top_n_outlier_pruning_block.log
+    rm --force --verbose --recursive Profiling{,.*}/
+}
+
+clean_testing() {
+    echo "Cleaning testing files..."
+    rm --force --verbose josh_test.log josh_test.*.log top_n_outlier_pruning_block.log
+    rm --force --verbose --recursive test/ test.*/
+}
+#-------------------------------------------------------------------------------
+
+#===============================================================================
+# Usage
+#===============================================================================
+usage() {
+    cat << EOF
+usage:
+    $0 [MODES...]
+
+This script cleans out the MATLAB directory.
+
+MODES
+    all
+    matlab
+    profiling
+    testing
+EOF
+}
+#-------------------------------------------------------------------------------
+
+if [[ $# -eq 0 ]]; then
+    echo "No mode specified." >&2
+    echo
+    usage
+fi
+
+for opt in $@; do
+    case $opt in
+        matlab)
+            clean_matlab
+            ;;
+            
+        profiling)
+            clean_profiling
+            ;;
+        
+        testing)
+            clean_testing
+            ;;
+        
+        all)
+            clean_matlab
+            clean_profiling
+            clean_testing
+            ;;
+        
+       *)
+	        # terminate while loop
+	        break
+	        ;;
+
+    esac
+done
