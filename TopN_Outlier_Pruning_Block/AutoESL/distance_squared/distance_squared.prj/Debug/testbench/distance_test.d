23384be9877ebd2c9d3a7eced65baa45d01413c4
Joshua Spence <josh@joshuaspence.com>
2012-08-22 00:44:12 +1000

    Removed unneeded AutoESL sample project.

--- TopN_Outlier_Pruning_Block/AutoESL/distance_squared/distance_squared.prj/Debug/testbench/distance_test.d
+++
@@ -1,5 +0,0 @@
-testbench/distance_test.d testbench/distance_test.o: \
- C:/Users/Joshua/Desktop/AutoESL/TopN_Outlier_Pruning_Block/distance_test.c \
- C:/Users/Joshua/Desktop/AutoESL/TopN_Outlier_Pruning_Block/distance.h
-
-C:/Users/Joshua/Desktop/AutoESL/TopN_Outlier_Pruning_Block/distance.h:
