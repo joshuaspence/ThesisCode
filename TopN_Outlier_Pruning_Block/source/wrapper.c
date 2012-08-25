1da7f05f1b0cf1eb86d90bcf7bd3d7025c44786f
Joshua Spence <josh@joshuaspence.com>
2012-08-25 17:26:14 +1000

    Changed hardcoded location of data files to reflect new folder structure.

--- TopN_Outlier_Pruning_Block/source/wrapper.c
+++ TopN_Outlier_Pruning_Block/source/wrapper.c
@@ -14,27 +14,27 @@
 /* All data sets                                                              */
 /*============================================================================*/
 static const char * const all_datasets[] = {
-    "../data/testoutrank.dat",
-    "../data/ball1.dat",
-    "../data/testCD.dat",
-    "../data/runningex1k.dat",
-    "../data/testCDST2.dat",
-    "../data/testCDST3.dat",
-    "../data/testCDST.dat",
-    "../data/runningex10k.dat",
-    "../data/runningex20k.dat",
-    "../data/segmentation.dat",
-    "../data/runningex30k.dat"
-    "../data/pendigits.dat",
-    "../data/runningex40k.dat",
-    "../data/spam_train.dat",
-    "../data/runningex50k.dat",
-    "../data/spam.dat",
-    "../data/letter-recognition.dat",
-    "../data/mesh_network.dat",
-    "../data/magicgamma.dat",
-    "../data/musk.dat",
-    "../data/connect4.dat"
+    "data/testoutrank.dat",
+    "data/ball1.dat",
+    "data/testCD.dat",
+    "data/runningex1k.dat",
+    "data/testCDST2.dat",
+    "data/testCDST3.dat",
+    "data/testCDST.dat",
+    "data/runningex10k.dat",
+    "data/runningex20k.dat",
+    "data/segmentation.dat",
+    "data/runningex30k.dat"
+    "data/pendigits.dat",
+    "data/runningex40k.dat",
+    "data/spam_train.dat",
+    "data/runningex50k.dat",
+    "data/spam.dat",
+    "data/letter-recognition.dat",
+    "data/mesh_network.dat",
+    "data/magicgamma.dat",
+    "data/musk.dat",
+    "data/connect4.dat"
 };
 /*----------------------------------------------------------------------------*/
 
