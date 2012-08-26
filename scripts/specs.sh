5f9cd5d124edc40092d536bc10c6be9137be33fd
Joshua Spence <josh@joshuaspence.com>
2012-08-27 06:51:04 +1000

    echo_and_execute function in specs script wasn't working.

--- scripts/specs.sh
+++ scripts/specs.sh
@@ -3,35 +3,30 @@
 OUTPUT_DIRECTORY=specs
 MATLAB_DIR=$(matlab -e 2>/dev/null | sed -n 's/MATLAB=\(.\+\)/\1/p')
 
-function echo_and_execute {
-    echo "$1 > $2"
-    $1 > $2
-}
-
 echo "Creating output directory..."
 rm -rf $OUTPUT_DIRECTORY
 mkdir --parents $OUTPUT_DIRECTORY
 
 echo "Running lshw..."
-echo_and_execute "sudo lshw -html" "$OUTPUT_DIRECTORY/specs.html"
+sudo lshw -html > $OUTPUT_DIRECTORY/specs.html
 echo
 
 echo "Getting gcc version..."
-echo_and_execute "gcc --version" "$OUTPUT_DIRECTORY/gcc.txt"
+gcc --version > $OUTPUT_DIRECTORY/gcc.txt
 echo
 
 echo "Getting MATLAB version..."
-echo_and_execute "matlab -nosplash -nodesktop -r \"ver; quit\"" "$OUTPUT_DIRECTORY/matlab.txt"
+matlab -nosplash -nodesktop -r "ver; quit" > $OUTPUT_DIRECTORY/matlab.txt
 echo
 
 echo "Getting mex info..."
-echo_and_execute "$MATLAB_DIR/bin/mex -v" "$OUTPUT_DIRECTORY/mex.txt"
+$MATLAB_DIR/bin/mex -v 2>/dev/null > $OUTPUT_DIRECTORY/mex.txt
 echo
 
 echo "Getting ld version..."
-echo_and_execute "ld -v" "$OUTPUT_DIRECTORY/ld.txt"
+ld -v > $OUTPUT_DIRECTORY/ld.txt
 echo
 
 echo "Getting OS info..."
-echo_and_execute "uname -a" "$OUTPUT_DIRECTORY/uname.txt"
+uname -a > $OUTPUT_DIRECTORY/uname.txt
 echo
