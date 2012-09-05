7c66b1f98fbbbd0812f49e4cb6e053d336a3980a
Joshua Spence <josh@joshuaspence.com>
2012-09-06 08:43:26 +1000

    Improvements to block size profiling plots.

--- scripts/gnuplot.sh
+++ scripts/gnuplot.sh
@@ -36,10 +36,7 @@ BLOCKSIZE_NAMES=( $(cat $DATA_FILE | awk --field-separator ',' "{ if (NR!=1) { p
 BLOCKSIZE_COLOURS=( $(for i in $(seq 0 ${#BLOCKSIZE_NAMES[*]}); do echo "$(expr 1 + $i)"; done) )
 ALL_BLOCKSIZES=( $(seq 0 $(expr ${#BLOCKSIZE_NAMES[*]} - 1) ) )
 
-#gnuplot -persist 
-cat <<- EOF
-#!/usr/bin/gnuplot
-
+gnuplot -persist <<- EOF
 reset
 set terminal tikz size 8.89cm,6.65cm color
 set datafile separator ","
@@ -61,7 +58,6 @@ set xlabel "Block size"
 set format x "\$10^{%L}\$"
 set ylabel "Total execution time (normalised)"
 set autoscale
-set key right center
 unset key
 
 set output "total_execution_time.tex"
@@ -71,7 +67,7 @@ plot \
         COLOUR=${DATASET_COLOURS[$i]}; \
         NO_BLOCKING_VALUE=$(cat $DATA_FILE | awk --field-separator ','  "{ gsub(/\"/,\"\",\$$COL_DATASET); if (NR!=1 && \$$COL_DATASET==\"$DATASET\" && \$$COL_BLOCKSIZE==$NO_BLOCKING_BLOCKSIZE) { print \$$COL_TOTALTIME_NORM } }"); \
         \
-        echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_TOTALTIME_NORM : 1/0) title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
+        echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_TOTALTIME_NORM : 1/0) smooth unique title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
         echo "$NO_BLOCKING_VALUE title '$DATASET (no blocking)' with line lt 0 lc $COLOUR, \\"; \
     done)
     1/0
@@ -84,7 +80,6 @@ set xlabel "Block size"
 set format x "\$10^{%L}\$"
 set ylabel "Function execution time (normalised)"
 set autoscale
-set key right center
 unset key
 
 set output "function_execution_time.tex"
@@ -94,7 +89,7 @@ plot \
         COLOUR=${DATASET_COLOURS[$i]}; \
         NO_BLOCKING_VALUE=$(cat $DATA_FILE | awk --field-separator ','  "{ gsub(/\"/,\"\",\$$COL_DATASET); if (NR!=1 && \$$COL_DATASET==\"$DATASET\" && \$$COL_BLOCKSIZE==$NO_BLOCKING_BLOCKSIZE) { print \$$COL_FUNCTIME_NORM } }"); \
         \
-        echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_FUNCTIME_NORM : 1/0) title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
+        echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_FUNCTIME_NORM : 1/0) smooth unique title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
         echo "$NO_BLOCKING_VALUE title '$DATASET (no blocking)' with line lt 0 lc $COLOUR, \\"; \
     done)
     1/0
@@ -108,7 +103,7 @@ set format x "\$10^{%L}\$"
 set ylabel "Calls to the distance function (normalised)"
 set autoscale
 set key right center
-unset key
+#unset key
 
 set output "distance_calls.tex"
 plot \
@@ -117,7 +112,7 @@ plot \
         COLOUR=${DATASET_COLOURS[$i]}; \
         NO_BLOCKING_VALUE=$(cat $DATA_FILE | awk --field-separator ','  "{ gsub(/\"/,\"\",\$$COL_DATASET); if (NR!=1 && \$$COL_DATASET==\"$DATASET\" && \$$COL_BLOCKSIZE==$NO_BLOCKING_BLOCKSIZE) { print \$$COL_DISTCALLS_NORM } }"); \
         \
-        echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_DISTCALLS_NORM : 1/0) title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
+        echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_DISTCALLS_NORM : 1/0) smooth unique title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
         echo "$NO_BLOCKING_VALUE title '$DATASET (no blocking)' with line lt 0 lc $COLOUR, \\"; \
     done)
     1/0
@@ -130,7 +125,6 @@ set xlabel "Block size"
 set format x "\$10^{%L}\$"
 set ylabel "Number of vectors pruned (normalised)"
 set autoscale
-set key right center
 unset key
 
 set output "vectors_pruned.tex"
@@ -140,7 +134,7 @@ plot \
         COLOUR=${DATASET_COLOURS[$i]}; \
         NO_BLOCKING_VALUE=$(cat $DATA_FILE | awk --field-separator ','  "{ gsub(/\"/,\"\",\$$COL_DATASET); if (NR!=1 && \$$COL_DATASET==\"$DATASET\" && \$$COL_BLOCKSIZE==$NO_BLOCKING_BLOCKSIZE) { print \$$COL_PRUNED_NORM } }"); \
         \
-        echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_PRUNED_NORM : 1/0) title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
+        echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_PRUNED_NORM : 1/0) smooth unique title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
         echo "$NO_BLOCKING_VALUE title '$DATASET (no blocking)' with line lt 0 lc $COLOUR, \\"; \
     done)
     1/0
@@ -153,7 +147,6 @@ set xlabel "Problem size"
 set format x "\$10^{%L}\$"
 set ylabel "Execution time"
 set autoscale
-set key right center
 unset key
 
 set output "total_run_time_complexity.tex"
@@ -163,7 +156,7 @@ plot \
         BLOCKSIZE_NAME="block_size=$BLOCKSIZE_VALUE"; \
         COLOUR=${BLOCKSIZE_COLOURS[$i]}; \
         \
-        echo "'$DATA_FILE' using $COL_VECTORS:(\$$COL_BLOCKSIZE == $BLOCKSIZE_VALUE ? \$$COL_TOTALTIME : 1/0) title '$BLOCKSIZE_NAME' with linespoints lt 1 lc $COLOUR, \\"; \
+        echo "'$DATA_FILE' using $COL_VECTORS:(\$$COL_BLOCKSIZE == $BLOCKSIZE_VALUE ? \$$COL_TOTALTIME : 1/0) smooth unique title '$BLOCKSIZE_NAME' with linespoints lt 1 lc $COLOUR, \\"; \
     done)
     1/0
 
@@ -175,7 +168,6 @@ set xlabel "Problem size"
 set format x "\$10^{%L}\$"
 set ylabel "Execution time"
 set autoscale
-set key right center
 unset key
 
 set output "function_run_time_complexity.tex"
@@ -185,7 +177,7 @@ plot \
         BLOCKSIZE_NAME="block_size=$BLOCKSIZE_VALUE"; \
         COLOUR=${BLOCKSIZE_COLOURS[$i]}; \
         \
-        echo "'$DATA_FILE' using $COL_VECTORS:(\$$COL_BLOCKSIZE == $BLOCKSIZE_VALUE ? \$$COL_FUNCTIME : 1/0) title '$BLOCKSIZE_NAME' with linespoints lt 1 lc $COLOUR, \\"; \
+        echo "'$DATA_FILE' using $COL_VECTORS:(\$$COL_BLOCKSIZE == $BLOCKSIZE_VALUE ? \$$COL_FUNCTIME : 1/0) smooth unique title '$BLOCKSIZE_NAME' with linespoints lt 1 lc $COLOUR, \\"; \
     done)
     1/0
 EOF
