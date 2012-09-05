960609e04cee96ad8a956dd7f1bf94bbd995e5f8
Joshua Spence <josh@joshuaspence.com>
2012-09-06 07:30:18 +1000

    Added total run time complexity and function run time complexity plots.

--- scripts/gnuplot.sh
+++ scripts/gnuplot.sh
@@ -31,9 +31,13 @@ COL_PRUNED_NORM=15
 DATASET_NAMES=( $(cat $DATA_FILE | awk --field-separator ',' "{ if (NR!=1) { print \$$COL_DATASET } }" | sed -e 's/"\(.\+\)"/\1/' | uniq) )
 DATASET_COLOURS=( $(for i in $(seq 0 ${#DATASET_NAMES[*]}); do echo "$(expr 1 + $i)"; done) )
 ALL_DATASETS=( $(seq 0 $(expr ${#DATASET_NAMES[*]} - 1) ) )
-BLOCKSIZES=$(cat $DATA_FILE | awk --field-separator ',' "{ if (NR!=1) { print \$$COL_BLOCKSIZE } }" | sed -e "/^$NO_BLOCKING_BLOCKSIZE\$/d" | sort --numeric-sort | uniq)
 
-gnuplot -persist <<- EOF
+BLOCKSIZE_NAMES=( $(cat $DATA_FILE | awk --field-separator ',' "{ if (NR!=1) { print \$$COL_BLOCKSIZE } }" | sed -e "/^$NO_BLOCKING_BLOCKSIZE\$/d" | sort --numeric-sort | uniq) )
+BLOCKSIZE_COLOURS=( $(for i in $(seq 0 ${#BLOCKSIZE_NAMES[*]}); do echo "$(expr 1 + $i)"; done) )
+ALL_BLOCKSIZES=( $(seq 0 $(expr ${#BLOCKSIZE_NAMES[*]} - 1) ) )
+
+#gnuplot -persist 
+cat <<- EOF
 #!/usr/bin/gnuplot
 
 reset
@@ -44,7 +48,6 @@ set datafile separator ","
 # Remove border on top and right and set color to gray
 set style line 11 lc rgb '#808080' lt 1
 set border 3 back ls 11
-set tics nomirror
 
 # Style definitions
 set style line 1 lc rgb '#8b1a0e' pt 1 ps 1 lt 1 lw 2 # --- red
@@ -139,6 +142,50 @@ plot \
         \
         echo "'$DATA_FILE' using (\$$COL_BLOCKSIZE != 0 ? \$$COL_BLOCKSIZE : 1/0):(stringcolumn($COL_DATASET) eq '$DATASET' ? \$$COL_PRUNED_NORM : 1/0) title '$DATASET' with linespoints lt 1 lc $COLOUR, \\"; \
         echo "$NO_BLOCKING_VALUE title '$DATASET (no blocking)' with line lt 0 lc $COLOUR, \\"; \
+    done)
+    1/0
+
+################################################################################
+# TOTAL RUN TIME COMPLEXITY
+################################################################################
+set log x
+set xlabel "Problem size"
+set format x "\$10^{%L}\$"
+set ylabel "Execution time"
+set autoscale
+set key right center
+unset key
+
+set output "total_run_time_complexity.tex"
+plot \
+    $(for i in ${ALL_BLOCKSIZES[*]}; do \
+        BLOCKSIZE_VALUE=${BLOCKSIZE_NAMES[$i]}; \
+        BLOCKSIZE_NAME="block_size=$BLOCKSIZE_VALUE"; \
+        COLOUR=${BLOCKSIZE_COLOURS[$i]}; \
+        \
+        echo "'$DATA_FILE' using $COL_VECTORS:(\$$COL_BLOCKSIZE == $BLOCKSIZE_VALUE ? \$$COL_TOTALTIME : 1/0) title '$BLOCKSIZE_NAME' with linespoints lt 1 lc $COLOUR, \\"; \
+    done)
+    1/0
+
+################################################################################
+# FUNCTION RUN TIME COMPLEXITY
+################################################################################
+set log x
+set xlabel "Problem size"
+set format x "\$10^{%L}\$"
+set ylabel "Execution time"
+set autoscale
+set key right center
+unset key
+
+set output "function_run_time_complexity.tex"
+plot \
+    $(for i in ${ALL_BLOCKSIZES[*]}; do \
+        BLOCKSIZE_VALUE=${BLOCKSIZE_NAMES[$i]}; \
+        BLOCKSIZE_NAME="block_size=$BLOCKSIZE_VALUE"; \
+        COLOUR=${BLOCKSIZE_COLOURS[$i]}; \
+        \
+        echo "'$DATA_FILE' using $COL_VECTORS:(\$$COL_BLOCKSIZE == $BLOCKSIZE_VALUE ? \$$COL_FUNCTIME : 1/0) title '$BLOCKSIZE_NAME' with linespoints lt 1 lc $COLOUR, \\"; \
     done)
     1/0
 EOF
