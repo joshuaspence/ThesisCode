c23b7f7e0a6a484594f17b7c803bb9edf2fc27fb
Joshua Spence <josh@joshuaspence.com>
2012-09-12 21:21:17 +1000

    Added ModelSim directory to autoesl_setup script

--- scripts/autoesl_setup.sh
+++ scripts/autoesl_setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+################################################################################
+#
+# Script to setup various environment variables necessary for using AutoESL with
+# the university's "tuna" FLEXlm licensing server.
+#
+# Usage:
+#     source autoesl_setup.sh
+#
+################################################################################
+
 function echo_and_exec {
 	echo $@
 	$@
@@ -17,10 +27,11 @@ AUTOESL_COMMON_DIR=$XILINX_DIR/2012.1/common
 EDK_DIR=$XILINX_DIR/13.2/ISE_DS/EDK
 EDK_COMMON_DIR=$XILINX_DIR/13.2/ISE_DS/common
 
-XILIND_LICENSE_SERVER=129.78.98.100
-XILIND_LICENSE_PORT=2100
-LOCALHOST_LICENSE_PORT=2100
-SSH_USER=joshua
+ALTERA_DIR=/opt/altera
+MODELSIM_DIR=$ALTERA_DIR/12.0/modelsim_ase
+
+XILINXD_LICENSE_SERVER=129.78.98.100
+XILINXD_LICENSE_PORT=2100
 
 echo "==============================================================================="
 echo "Make sure that you sourced this file, using 'source $0'."
@@ -28,8 +39,14 @@ echo "==========================================================================
 echo ""
 
 # Check that AUTOESL_DIR is set correctly
-if [[ ! -x $AUTOESL_DIR/bin/autoesl ]]; then
+if [ ! -x $AUTOESL_DIR/bin/autoesl ]; then
     echo "autoesl executable not found in '$AUTOESL_DIR'" >&2
+    exit 1
+fi
+
+# Check that MODELSIM_DIR is set correctly
+if [ ! -x $MODELSIM_DIR/bin/vsim ]; then
+    echo "vsim executable not found in '$MODELSIM_DIR'" >&2
     exit 1
 fi
 
@@ -38,16 +55,16 @@ pathadd $AUTOESL_DIR/bin
 pathadd $AUTOESL_COMMON_DIR/bin/lin64
 pathadd $EDK_DIR/bin/lin64
 pathadd $EDK_COMMON_DIR/bin/lin64
+
+# Add Modelsim to the current path
+pathadd $MODELSIM_DIR/bin
+
 echo "PATH=$PATH"
 echo ""
 
-# Set up SSH tunnel
-#echo_and_exec ssh -f -N -L $LOCALHOST_LICENSE_PORT:127.0.0.1:$XILIND_LICENSE_PORT $SSH_USER@$XILIND_LICENSE_SERVER
-#echo ""
-
 # Point to the university licensing server (tunneled to localhost)
-echo_and_exec export XILINXD_LICENSE_FILE=$XILIND_LICENSE_PORT@$XILIND_LICENSE_SERVER
-echo_and_exec export LM_LICENSE_FILE=$XILIND_LICENSE_PORT@$XILIND_LICENSE_SERVER
+echo_and_exec export XILINXD_LICENSE_FILE=$XILINXD_LICENSE_PORT@$XILINXD_LICENSE_SERVER
+echo_and_exec export LM_LICENSE_FILE=$XILINXD_LICENSE_PORT@$XILINXD_LICENSE_SERVER
 echo ""
 
 echo "Done!"
