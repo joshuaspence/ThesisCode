c49938104992ebc45b0b40199e4a1c85dc988199
Joshua Spence <josh@joshuaspence.com>
2012-10-05 15:16:12 +1000

    Added some Windows specific stuff. Also created PlanAhead project.

---
+++ TopN_Outlier_Pruning_Block/FPGA/PlanAhead/NO_BLOCKING/NO_BLOCKING.srcs/sources_1/edk/system/system_stub.v
@@ -0,0 +1,78 @@
+//-----------------------------------------------------------------------------
+// system_stub.v
+//-----------------------------------------------------------------------------
+
+module system_stub
+  (
+    processing_system7_0_MIO,
+    processing_system7_0_PS_SRSTB,
+    processing_system7_0_PS_CLK,
+    processing_system7_0_PS_PORB,
+    processing_system7_0_DDR_Clk,
+    processing_system7_0_DDR_Clk_n,
+    processing_system7_0_DDR_CKE,
+    processing_system7_0_DDR_CS_n,
+    processing_system7_0_DDR_RAS_n,
+    processing_system7_0_DDR_CAS_n,
+    processing_system7_0_DDR_WEB_pin,
+    processing_system7_0_DDR_BankAddr,
+    processing_system7_0_DDR_Addr,
+    processing_system7_0_DDR_ODT,
+    processing_system7_0_DDR_DRSTB,
+    processing_system7_0_DDR_DQ,
+    processing_system7_0_DDR_DM,
+    processing_system7_0_DDR_DQS,
+    processing_system7_0_DDR_DQS_n,
+    processing_system7_0_DDR_VRN,
+    processing_system7_0_DDR_VRP
+  );
+  inout [53:0] processing_system7_0_MIO;
+  input processing_system7_0_PS_SRSTB;
+  input processing_system7_0_PS_CLK;
+  input processing_system7_0_PS_PORB;
+  inout processing_system7_0_DDR_Clk;
+  inout processing_system7_0_DDR_Clk_n;
+  inout processing_system7_0_DDR_CKE;
+  inout processing_system7_0_DDR_CS_n;
+  inout processing_system7_0_DDR_RAS_n;
+  inout processing_system7_0_DDR_CAS_n;
+  output processing_system7_0_DDR_WEB_pin;
+  inout [2:0] processing_system7_0_DDR_BankAddr;
+  inout [14:0] processing_system7_0_DDR_Addr;
+  inout processing_system7_0_DDR_ODT;
+  inout processing_system7_0_DDR_DRSTB;
+  inout [31:0] processing_system7_0_DDR_DQ;
+  inout [3:0] processing_system7_0_DDR_DM;
+  inout [3:0] processing_system7_0_DDR_DQS;
+  inout [3:0] processing_system7_0_DDR_DQS_n;
+  inout processing_system7_0_DDR_VRN;
+  inout processing_system7_0_DDR_VRP;
+
+  (* BOX_TYPE = "user_black_box" *)
+  system
+    system_i (
+      .processing_system7_0_MIO ( processing_system7_0_MIO ),
+      .processing_system7_0_PS_SRSTB ( processing_system7_0_PS_SRSTB ),
+      .processing_system7_0_PS_CLK ( processing_system7_0_PS_CLK ),
+      .processing_system7_0_PS_PORB ( processing_system7_0_PS_PORB ),
+      .processing_system7_0_DDR_Clk ( processing_system7_0_DDR_Clk ),
+      .processing_system7_0_DDR_Clk_n ( processing_system7_0_DDR_Clk_n ),
+      .processing_system7_0_DDR_CKE ( processing_system7_0_DDR_CKE ),
+      .processing_system7_0_DDR_CS_n ( processing_system7_0_DDR_CS_n ),
+      .processing_system7_0_DDR_RAS_n ( processing_system7_0_DDR_RAS_n ),
+      .processing_system7_0_DDR_CAS_n ( processing_system7_0_DDR_CAS_n ),
+      .processing_system7_0_DDR_WEB_pin ( processing_system7_0_DDR_WEB_pin ),
+      .processing_system7_0_DDR_BankAddr ( processing_system7_0_DDR_BankAddr ),
+      .processing_system7_0_DDR_Addr ( processing_system7_0_DDR_Addr ),
+      .processing_system7_0_DDR_ODT ( processing_system7_0_DDR_ODT ),
+      .processing_system7_0_DDR_DRSTB ( processing_system7_0_DDR_DRSTB ),
+      .processing_system7_0_DDR_DQ ( processing_system7_0_DDR_DQ ),
+      .processing_system7_0_DDR_DM ( processing_system7_0_DDR_DM ),
+      .processing_system7_0_DDR_DQS ( processing_system7_0_DDR_DQS ),
+      .processing_system7_0_DDR_DQS_n ( processing_system7_0_DDR_DQS_n ),
+      .processing_system7_0_DDR_VRN ( processing_system7_0_DDR_VRN ),
+      .processing_system7_0_DDR_VRP ( processing_system7_0_DDR_VRP )
+    );
+
+endmodule
+
---
+++ TopN_Outlier_Pruning_Block/source/test.cpp
@@ -0,0 +1,121 @@
+/*============================================================================*/
+/* Includes                                                                   */
+/*============================================================================*/
+#include "checks.h" /* check for invalid preprocessor macro combinations */
+#include "arch.h" /* set architecture specific macros */
+
+#include "test.h" /* main header file */
+#include "top_n_outlier_pruning_block.h" /* for top_n_outlier_pruning_block */
+#include "utility.h" /* for boolean, index_t, double_t, false, lint_t, PRINTF_STDOUT, size_t, true, uint_t */
+#include "vardump.h" /* for read_vardump */
+
+#include <stdlib.h> /* for free */
+/*----------------------------------------------------------------------------*/
+
+/*
+ * Reads variable from a variable dump file and runs the
+ * top_n_outlier_pruning_block algorithm on the input data. After the algorithm 
+ * completes, compares the actual output with the expected output.
+ *
+ * Parameters:
+ *     - data_file: The variable dump file to read the data from.
+ *
+ * Return:
+ *    Zero if the test was successful, otherwise 1.
+ */
+int test(const char * const data_file) {
+    boolean failed = false;
+       
+    size_t num_vectors;
+    size_t vector_dims;
+    double_t * data;
+    size_t k;
+    size_t N;
+    size_t block_size;
+    index_t * outliers_expected;
+    double_t * outlier_scores_expected;
+    
+    PRINTF_STDOUT("Reading variables from '%s'.\n", data_file); 
+    int result;
+        if ((result = read_vardump(data_file, &num_vectors, &vector_dims, &data, &k, &N, &block_size, &outliers_expected, &outlier_scores_expected)) != 0)
+        return result;
+    
+    /* Create the output arrays. */
+    index_t  outliers      [N];
+    double_t outlier_scores[N];
+    MEMSET_1D(outliers,       0, N, sizeof(index_t));
+    MEMSET_1D(outlier_scores, 0, N, sizeof(double_t));
+    
+    /* Call the function. */
+    PRINTF_STDOUT("Running top_n_outlier_pruning_block() for data set '%s'.\n", data_file);
+#ifndef HARDCODED_NUM_VECTORS
+    set_num_vectors(num_vectors);
+#endif /* #ifndef HARDCODED_NUM_VECTORS */
+#ifndef HARDCODED_VECTOR_DIMS
+    set_vector_dims(vector_dims);
+#endif /* #ifndef HARDCODED_VECTOR_DIMS */
+#ifndef HARDCODED_K
+    set_k(k);
+#endif /* #ifndef HARDCODED_K */
+#ifndef HARDCODED_N
+    set_N(N);
+#endif /* #ifndef HARDCODED_N */
+#if defined(BLOCKING) && !defined(HARDCODED_BLOCK_SIZE)
+    set_block_size(block_size);
+#endif /* #if defined(BLOCKING) && !defined(HARDCODED_BLOCK_SIZE) */
+    const UNUSED uint_t num_pruned = top_n_outlier_pruning_block((void *) data, outliers, outlier_scores);
+    
+    /* Compare outliers. */
+    do {
+        PRINTF_STDOUT("Comparing outliers... ");
+        uint_t i;
+        uint_t similarity = 0;
+        boolean used[N];
+        MEMSET_1D(used, false, N, sizeof(boolean));
+        for (i = 0; i < N; i++) {
+            uint_t j;
+            for (j = 0; j < N; j++) {
+                if (!used[j] && outliers_expected[i] == outliers[j]) {
+                    used[j] = true;
+                    similarity++;
+                    break;
+                }
+            }
+        }
+        
+        PRINTF_STDOUT("%u out of %u\n", (unsigned int) similarity, (unsigned int) N);
+        if (similarity != N)
+            failed = true;
+    } while (0);
+    
+    /* Compare scores. */
+    do {
+        PRINTF_STDOUT("Comparing scores... ");
+        uint_t i;
+        uint_t similarity = 0;
+        boolean used[N];
+        MEMSET_1D(used, false, N, sizeof(boolean));
+        for (i = 0; i < N; i++) {
+            uint_t j;
+            for (j = 0; j < N; j++) {
+                if (!used[j] && outlier_scores_expected[i] == outlier_scores[j]) {
+                    used[j] = true;
+                    similarity++;
+                    break;
+                }
+            }
+        }
+        
+        PRINTF_STDOUT("%u out of %u\n", (unsigned int) similarity, (unsigned int) N);
+        if (similarity != N)
+            failed = true;
+    } while (0);
+    
+    /* Free dynamic memory. */
+    free(data);
+    free(outliers_expected);
+    free(outlier_scores_expected);
+    
+    PRINTF_STDOUT("Number of pruned vectors = %u\n", num_pruned);
+    return failed;
+}
